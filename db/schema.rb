53fe6d8af4fc64f4190322eff923f969d4cdf89d
Kuniaki Igarashi <kuniaki.igarashi@garnettech373.com>
2022-09-28 18:17:14 +0900

    Writing FactoryBot with association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_21_093252) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_28_090942) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -18,4 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_21_093252) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "variations", force: :cascade do |t|
+    t.string "kind"
+    t.integer "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_variations_on_book_id"
+  end
+
+  add_foreign_key "variations", "books"
 end
